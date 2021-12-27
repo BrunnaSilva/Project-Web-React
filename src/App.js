bc6ab5de7fdd2b122c16b356e80371bea71a9139
brunnasilva <bruna.silva@popcode.com.br>
2021-12-27 19:02:05 -0300

    feat: (first project) notes list web

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ListaDeNotas from "./components/ListaDeNotas";
+import FormularioCadastro from "./components/FormularioCadastro";
+import ListaDeCategorias from "./components/ListaDeCategorias";
+import "./assets/App.css";
+import "./assets/index.css";
+import Categorias from "./data/Categorias";
+import Notas from "./data/Notas";
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  constructor() {
+    super();
+    this.categorias = new Categorias()
+    this.notas = new Notas()
+  }
+
+  render() {
+    return (
+      <section className="conteudo">
+        <FormularioCadastro
+          categorias={this.categorias}
+          criarNota={this.notas.adicionarNota.bind(this.notas)}
+        />
+        <main className="conteudo-principal">
+          <ListaDeCategorias
+            adicionarCategoria={this.categorias.adicionarCategoria.bind(this.categorias)}
+            categorias={this.categorias}
+          />
+          <ListaDeNotas
+            apagarNota={this.notas.apagarNota.bind(this.notas)}
+            notas={this.notas}
+          />
+        </main>
+      </section>
+    );
+  }
 }
 
 export default App;
+
+
+/*criarNota(titulo, texto, categoria) {
+    const novaNota = { titulo, texto, categoria };
+    const novoArrayNotas = [...this.state.notas, novaNota];
+    const novoEstado = {
+      notas: novoArrayNotas,
+    };
+    this.setState(novoEstado);
+  }
+
+  adicionarCategoria(nomeCategoria) {
+    const novoArrayCategorias = [...this.state.categorias, nomeCategoria];
+    const novoEstado = { ...this.state, categorias: novoArrayCategorias };
+    this.setState(novoEstado);
+  }
+
+  deletarNota(index) {
+    let arrayNotas = this.state.notas;
+    arrayNotas.splice(index, 1);
+    this.setState({ notas: arrayNotas });
+  }*/
---
+++ src/components/ListaDeNotas/ListaDeNotas.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import CardNota from "../CardNota";
+import "./estilo.css";
+class ListaDeNotas extends Component {
+
+  constructor(){
+    super()
+    this.state = { notas: []}
+
+    this._newNotes = this._newNotes.bind(this)
+  }
+ 
+  componentDidMount(){
+    this.props.notas.inscrever(this._newNotes)
+  }
+
+  componentWillUnmount(){
+    this.props.notas.desinscrever(this._newNotes)
+  }
+
+  _newNotes(notas){
+    this.setState({...this.state, notas})
+  }
+
+  render() {
+    return (
+      <ul className="lista-notas">
+        {this.state.notas.map((nota, index) => {
+          return (
+            <li className="lista-notas_item" key={index}>
+              
+              <CardNota 
+              indice={index}
+              apagarNota={this.props.apagarNota}
+              titulo={nota.titulo} 
+              texto={nota.texto}
+              categoria={nota.categoria}
+              />
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
+}
+
+export default ListaDeNotas;
